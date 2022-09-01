5a59116af088a45c3787dc759b8e025bcc1ec870
terry <terry160@tutanota.com>
2022-09-01 18:44:47 +0100

    you know who else

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const { Client, GatewayIntentBits, Partials, EmbedBuilder, Colors, GuildMember, PartialGuildMember, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const noblox = require("noblox.js"); // See client.once('ready') block
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const token = process.env.BOT_TOKEN;
 const qOSKey = process.env.QOS_KEY;
@@ -9,6 +10,14 @@ const qsstGroupID = "5681740";
 const sstAdminRoleID = "986687516591677510";
 const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers];
 const partials = [Partials.GuildMember];
+
+// Stuff we need
+const supporterRoleIDs = ["1009885228023689226", "1009885281014513785", "1009885317291057182"];
+const logChannelID = "1009515522972459068";
+const qspID = "986685866661527562";
+
+let logChannel = null
+let qsp = null
 
 const client = new Client({ intents: intents, partials: partials });
 
@@ -75,65 +84,80 @@ async function handleUserAcceptance(member) {
 
     try {
         // Terminate early using the catch because I don't wanna write another embed
-        if (robloxID === "Not found within qOS") throw new Error();
+        if (robloxID === "Not found within qOS") throw new Error(robloxID);
 
         let request = await noblox.getJoinRequest(qsstGroupID, robloxID);
+
         if (request != null) {
             // User is pending
             const embed = new EmbedBuilder()
-                .setTitle("New Subscriber is Pending")
-                .setDescription("A new subscriber is already pending to the QSST group. Vetting may begin.")
-                .setColor(Colors.DarkBlue)
-                .addFields(
-                    { name: "User", value: `${member.user.tag} (${member.id})` },
-                    { name: "Roblox name", value: robloxName },
-                );
-
-            return await logChannel.send({ content: `<@&${sstAdminRoleID}>`, embeds: [embed] });
-        }
-
-        // User is not pending, so send an info message
-
-        const embed = new EmbedBuilder()
-            .setTitle("Waiting for New Subscriber to Pend")
-            .setDescription("A new user has subscribed but is not pending to the QSST group.")
-            .setColor(Colors.Blue)
+            .setTitle("New Subscriber is Pending")
+            .setDescription("A new subscriber is already pending to the QSST group. Vetting may begin.")
+            .setColor(Colors.DarkBlue)
             .addFields(
                 { name: "User", value: `${member.user.tag} (${member.id})` },
                 { name: "Roblox name", value: robloxName },
             );
+
+            const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+				.setLabel('Accept into QSST')
+                .setCustomId(`Accept_${robloxID}`)
+				.setStyle(ButtonStyle.Success),
+
+                new ButtonBuilder()
+				.setLabel('Decline request')
+                .setCustomId(`Decline_${robloxID}`)
+				.setStyle(ButtonStyle.Danger)
+			);
+
+            return await logChannel.send({ /* content: `<@&${sstAdminRoleID}>` , */ embeds: [embed], components: [row] });
+        }
+
+        // User is not pending, so send an info message
+        const embed = new EmbedBuilder()
+        .setTitle("Waiting for New Subscriber to Pend")
+        .setDescription("A new user has subscribed but is not pending to the QSST group.")
+        .setColor(Colors.Blue)
+        .addFields(
+            { name: "User", value: `${member.user.tag} (${member.id})` },
+            { name: "Roblox name", value: robloxName },
+        );
 
         await logChannel.send({ embeds: [embed] });
 
         // Try to inform the user to submit a join request
         try {
             const memberNotifEmbed = new EmbedBuilder()
-                .setTitle("New Supporter Notification")
-                .setDescription(
-                    "Hello! Thank you for supporting Quantum with your purchase of a QSP premium membership."
-                        + "\nI noticed that you aren't pending to join the QSST group. Please do so here."
-                        + "\nhttps://www.roblox.com/groups/5681740/Quantum-Structural-Science-Team"
-                        + "\n\nYou will be vetted once you submit your join request."
-                )
-                .setColor(Colors.Blue);
-            return await member.send({ embeds: [embed] });
+            .setTitle("New Supporter Notification")
+            .setColor(Colors.Blue)
+            .setDescription(
+                "Hello! Thank you for supporting Quantum with your purchase of a QSP premium membership."
+                    + "\nI noticed that you aren't pending to join the QSST group. Please do so here."
+                    + "\nhttps://www.roblox.com/groups/5681740/Quantum-Structural-Science-Team"
+                    + "\n\nYou will be vetted once you submit your join request."
+            );
+
+            return await member.send({ embeds: [memberNotifEmbed] });
         } catch (err) {
             return await logChannel.send(`Failed to notify ${member.user.tag} to request to join the SST group.`)
         }
-
     } catch (err) {
+        console.log(`Error handling subscribe for ${member.user.tag} (${member.id}) | ${err}`)
+
         // Something failed so tell SST Admins
         const embed = new EmbedBuilder()
-            .setTitle("New Supporter Handling Failed")
-            .setDescription("Failed to automatically handle a new supporter. Please do this manually.")
-            .setColor(Colors.DarkRed)
-            .addFields(
-                { name: "User", value: `${member.user.tag} (${member.id})` },
-                { name: "Roblox name", value: robloxName },
-            );
-        return await logChannel.send({ content: `<@&${sstAdminRoleID}>`, embeds: [embed] });
-    }
+        .setTitle("New Supporter Handling Failed")
+        .setDescription("Failed to automatically handle a new supporter. Please do this manually.")
+        .setColor(Colors.DarkRed)
+        .addFields(
+            { name: "User", value: `${member.user.tag} (${member.id})` },
+            { name: "Roblox name", value: robloxName },
+        );
 
+        return await logChannel.send({ /* content: `<@&${sstAdminRoleID}>` , */  embeds: [embed] });
+    }
 }
 
 /**
@@ -147,19 +171,20 @@ async function handleUserKick(member) {
 
     try {
         // Terminate early using catch block because I don't want to write another embed
-        if (robloxID === "Not found within qOS") throw new Error();
+        if (robloxID === "Not found within qOS") throw new Error(robloxID);
 
         // Check if user is even in QSST or if they left already
         let rankInSST = await noblox.getRankInGroup(qsstGroupID, robloxID);
+
         if (rankInSST === 0) {
             const embed = new EmbedBuilder()
-                .setTitle("Automatic SST User Removal")
-                .setDescription("User who unsubscribed was not in QSST group. No further action should be necessary.")
-                .setColor(Colors.Blue)
-                .addFields(
-                    { name: "User", value: `${member.user.tag} (${member.id})` },
-                    { name: "Roblox name", value: robloxName },
-                );
+            .setTitle("Automatic SST User Removal")
+            .setDescription("User who unsubscribed was not in QSST group. No further action should be necessary.")
+            .setColor(Colors.Blue)
+            .addFields(
+                { name: "User", value: `${member.user.tag} (${member.id})` },
+                { name: "Roblox name", value: robloxName },
+            );
 
             return await logChannel.send({ embeds: [embed] });
         }
@@ -168,47 +193,33 @@ async function handleUserKick(member) {
         await noblox.exile(qsstGroupID, robloxID);
 
         const embed = new EmbedBuilder()
-            .setTitle("Automatic SST User Removal")
-            .setDescription("User who unsubscribed was in SST group and was automatically removed. No further action should be necessary.")
-            .setColor(Colors.Blue)
-            .addFields(
-                { name: "User", value: `${member.user.tag} (${member.id})` },
-                { name: "Roblox name", value: robloxName },
-            );
+        .setTitle("Automatic SST User Removal")
+        .setDescription("User who unsubscribed was in SST group and was automatically removed. No further action should be necessary.")
+        .setColor(Colors.Blue)
+        .addFields(
+            { name: "User", value: `${member.user.tag} (${member.id})` },
+            { name: "Roblox name", value: robloxName },
+        );
 
         return await logChannel.send({ embeds: [embed] });
     } catch (err) {
+        console.log(`Error handling unsubscribe for ${member.user.tag} (${member.id}) | ${err}`)
+
         // Something failed so tell SST Admins
         const embed = new EmbedBuilder()
-            .setTitle("Automatic SST User Removal Failed")
-            .setDescription("Failed to automatically kick an unsubscribed supporter out of the QSST group. Please do this manually.")
-            .setColor(Colors.DarkRed)
-            .addFields(
-                { name: "User", value: `${member.user.tag} (${member.id})` },
-                { name: "Roblox name", value: robloxName },
-            );
-        return await logChannel.send({ content: `<@&${sstAdminRoleID}>`, embeds: [embed] });
+        .setTitle("Automatic SST User Removal Failed")
+        .setDescription("Failed to automatically kick an unsubscribed supporter out of the QSST group. Please do this manually.")
+        .setColor(Colors.DarkRed)
+        .addFields(
+            { name: "User", value: `${member.user.tag} (${member.id})` },
+            { name: "Roblox name", value: robloxName },
+        );
+
+        return await logChannel.send({ /* content: `<@&${sstAdminRoleID}>` , */  embeds: [embed] });
     }
-}
-
-client.once('ready', async () => {
-    // Log in with Noblox
-    // cue terry disapproving stare
-    // I couldn't get terry's other code to play nice so I took the easier route
-    const robloxClient = await noblox.setCookie(process.env.ROBLOSECURITY);
-    console.log(`Logged in as ${robloxClient.UserName}`);
-
-    console.log("ready!");
-});
+};
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
-    // Stuff we need
-    const supporterRoleIDs = ["1009885228023689226", "1009885281014513785", "1009885317291057182"];
-    const qspID = "986685866661527562";
-    const qsp = client.guilds.cache.get(qspID);
-    const logChannelID = "1009515522972459068";
-    const logChannel = client.channels.cache.get(logChannelID);
-
     // Check if user has role and if oldUser is a partial
     // oldUser partials only have the "@everyone" role, which is what the isPartial check looks for
     const hasRoleNow = supporterRoleIDs.find(roleID => newMember.roles.cache.has(roleID));
@@ -221,34 +232,31 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
 
         if (!hasRoleNow) {
             const embed = new EmbedBuilder()
-                .setTitle("User Support Status Inference")
-                .setDescription("A change in a user's support status occurred and I had to infer what happened. Please validate this if there is cause for concern.")
-                .setColor(Colors.Gold)
-                .addFields(
-                    { name: "User", value: `${newMember.user.tag} (${newMember.id})` },
-                    { name: "Roblox name", value: robloxName },
-                    { name: "Support Tier (guess)", value: "Stopped supporting" }
-                );
-
-            await logChannel.send({ embeds: [embed] });
-            await handleUserKick(newMember)
-            return;
-        }
-
-        const embed = new EmbedBuilder()
             .setTitle("User Support Status Inference")
             .setDescription("A change in a user's support status occurred and I had to infer what happened. Please validate this if there is cause for concern.")
             .setColor(Colors.Gold)
             .addFields(
                 { name: "User", value: `${newMember.user.tag} (${newMember.id})` },
                 { name: "Roblox name", value: robloxName },
-                { name: "Support Tier (guess)", value: qsp.roles.cache.get(hasRoleNow).name }
+                { name: "Support Tier (guess)", value: "Stopped supporting" }
             );
 
-        await logChannel.send({ embeds: [embed] });
-        await handleUserAcceptance(newMember);
+            await logChannel.send({ embeds: [embed] });
+            return await handleUserKick(newMember)
+        }
 
-        return;
+        const embed = new EmbedBuilder()
+        .setTitle("User Support Status Inference")
+        .setDescription("A change in a user's support status occurred and I had to infer what happened. Please validate this if there is cause for concern.")
+        .setColor(Colors.Gold)
+        .addFields(
+            { name: "User", value: `${newMember.user.tag} (${newMember.id})` },
+            { name: "Roblox name", value: robloxName },
+            { name: "Support Tier (guess)", value: qsp.roles.cache.get(hasRoleNow).name }
+        );
+
+        await logChannel.send({ embeds: [embed] });
+        return await handleUserAcceptance(newMember, true);
     }
 
     // oldUser isn't a partial, so check its roles too
@@ -257,34 +265,30 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
     if (!hadRole && hasRoleNow) {
         // Didn't have role before but has it now === new supporter!
         const embed = new EmbedBuilder()
-            .setTitle("New Supporter!")
-            .setDescription("There is a new person supporting QSP!")
-            .setColor(Colors.Green)
-            .addFields(
-                { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
-                { name: "Roblox name", value: robloxName },
-                { name: "Support Tier", value: qsp.roles.cache.get(hasRoleNow).name }
-            );
+        .setTitle("New Supporter!")
+        .setDescription("There is a new person supporting QSP!")
+        .setColor(Colors.Green)
+        .addFields(
+            { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
+            { name: "Roblox name", value: robloxName },
+            { name: "Support Tier", value: qsp.roles.cache.get(hasRoleNow).name }
+        );
 
         await logChannel.send({ embeds: [embed] });
-        await handleUserAcceptance(newMember);
-
-        return;
+        return await handleUserAcceptance(newMember);
     } else if (hadRole && !hasRoleNow) {
         // Had role before but doesn't now === no longer supporting :(
         const embed = new EmbedBuilder()
-            .setTitle("User Stopped Supporting")
-            .setDescription("Someone is no longer supporting QSP.")
-            .setColor(Colors.Red)
-            .addFields(
-                { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
-                { name: "Roblox name", value: robloxName },
-            );
+        .setTitle("User Stopped Supporting")
+        .setDescription("Someone is no longer supporting QSP.")
+        .setColor(Colors.Red)
+        .addFields(
+            { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
+            { name: "Roblox name", value: robloxName },
+        );
 
         await logChannel.send({ embeds: [embed] });
-        await handleUserKick(newMember);
-
-        return;
+        return await handleUserKick(newMember);
     }
 });
 
@@ -297,26 +301,52 @@ client.on('guildMemberRemove', async member => {
     const wasSupporter = supporterRoleIDs.some(roleID => member.roles.cache.has(roleID));
 
     // We don't care if they weren't supporting
-    if (!wasSupporter) { return; }
+    if (!wasSupporter) return;
 
     // Notify that a supporter left and handle the kick
     const embed = new EmbedBuilder()
-        .setTitle("User Stopped Supporting")
-        .setDescription("Someone is no longer supporting QSP.")
-        .setColor(Colors.Red)
-        .addFields(
-            { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
-            { name: "Roblox name", value: robloxName },
-        );
+    .setTitle("User Stopped Supporting")
+    .setDescription("Someone is no longer supporting QSP.")
+    .setColor(Colors.Red)
+    .addFields(
+        { name: "User", value: `${oldMember.user.tag} (${oldMember.id})` },
+        { name: "Roblox name", value: robloxName },
+    );
 
     await logChannel.send({ embeds: [embed] });
-    await handleUserKick(member);
-    return;
+    return await handleUserKick(member);
 });
 
-const joinRequestEvent = noblox.onJoinRequest(parseInt(qsstGroupID));
-joinRequestEvent.on("data", data => {
-    console.log(data);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+})
+
+async function JoinReqData(Data) {
+
+}
+
+async function JoinReqError(Err) {
+
+}
+
+client.once('ready', async () => {
+    // cuh
+    qsp = await client.guilds.cache.get(qspID);
+    logChannel = await client.channels.cache.get(logChannelID)
+
+    // Log in with Noblox
+    // cue terry disapproving stare
+    // I couldn't get terry's other code to play nice so I took the easier route
+    const robloxClient = await noblox.setCookie(process.env.ROBLOSECURITY);
+    console.log(`Logged in as ${robloxClient.UserName}`);
+
+    const joinRequestEvent = noblox.onJoinRequest(parseInt(qsstGroupID));
+
+    joinRequestEvent.on("data", JoinReqData);
+    joinRequestEvent.on("error", JoinReqError);
+
+    console.log("ready!");
 });
 
 client.login(token);
